911d3d6d7e8889c9aa2c0c2c5303caba0caab5cf
j-micevska <jana.micevska@gmail.com>
2021-12-26 14:22:48 +0100

    Dropdown menu za mapite

--- Domashna3/prototype/src/main/java/mk/finki/ukim/dians/web/controllers/MapController.java
+++ Domashna3/prototype/src/main/java/mk/finki/ukim/dians/web/controllers/MapController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
 
 @Controller
-@RequestMapping("/Map")
+@RequestMapping
 public class MapController {
 
     final AttractionService attractionService;
@@ -19,12 +19,25 @@ public class MapController {
         this.attractionService = attractionService;
     }
 
-    @GetMapping
-    public String getMap(Model model){
-        List<Attraction> attractionsSkopje=this.attractionService.findAllSkopje();
-        List<Attraction> attractionsOhrid=this.attractionService.findAllOhrid();
-        model.addAttribute("attractionsSkopje", attractionsSkopje);
-        model.addAttribute("attractionsOhrid", attractionsOhrid);
-        return "Map";
+//    @GetMapping
+//    public String getMap(Model model){
+//        List<Attraction> attractionsSkopje=this.attractionService.findAllSkopje();
+//        List<Attraction> attractionsOhrid=this.attractionService.findAllOhrid();
+//        model.addAttribute("attractionsSkopje", attractionsSkopje);
+//        model.addAttribute("attractionsOhrid", attractionsOhrid);
+//        return "Map";
+
+        @GetMapping("/MapSkopje")
+        public String getMapSkopje(Model model) {
+            List<Attraction> attractionsSkopje = this.attractionService.findAllSkopje();
+            model.addAttribute("attractionsSkopje", attractionsSkopje);
+            return "MapSkopje.html";
+        }
+
+        @GetMapping("/MapOhrid")
+        public String getMapOhrid(Model model){
+            List<Attraction> attractionsOhrid=this.attractionService.findAllOhrid();
+            model.addAttribute("attractionsOhrid", attractionsOhrid);
+            return "MapOhrid.html";
     }
 }
