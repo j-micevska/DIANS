983c3fa9f8153647a6b643be1c998a6b1897f77f
galenamilenkoska <94135872+galenamilenkoska@users.noreply.github.com>
2022-01-30 15:16:18 +0100

    Add files via upload

---
+++ Domasna 4/Gateway/src/test/java/mk/ukim/finki/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
