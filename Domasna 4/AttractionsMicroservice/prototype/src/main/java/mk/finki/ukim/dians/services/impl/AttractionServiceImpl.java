3d2a0fe504bb9fb4ca180ac6680fd339dd7ce104
galenamilenkoska <94135872+galenamilenkoska@users.noreply.github.com>
2022-01-30 15:07:30 +0100

    Add files via upload

---
+++ Domasna 4/AttractionsMicroservice/prototype/src/main/java/mk/finki/ukim/dians/services/impl/AttractionServiceImpl.java
@@ -0,0 +1,69 @@
+package mk.finki.ukim.dians.services.impl;
+
+import lombok.extern.slf4j.Slf4j;
+import mk.finki.ukim.dians.model.Attraction;
+import mk.finki.ukim.dians.repository.AttractionRepository;
+import mk.finki.ukim.dians.services.AttractionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+//@Slf4j
+public class AttractionServiceImpl implements AttractionService {
+
+    @Autowired
+    final AttractionRepository attractionRepository;
+
+    public AttractionServiceImpl(AttractionRepository attractionRepository) {
+        this.attractionRepository = attractionRepository;
+    }
+
+    @Override
+    public List<Attraction> findAllSkopje() {
+        return attractionRepository.findAllSkopje();
+    }
+    @Override
+    public List<Attraction> findAllOhrid() {
+        return attractionRepository.findAllOhrid();
+    }
+
+    @Override
+    public Optional<Attraction> findByIdSkopje(String id) {
+        return attractionRepository.findByIdSkopje(id);
+    }
+    @Override
+    public Optional<Attraction> findByIdOhrid(String id) {
+        return attractionRepository.findByIdOhrid(id);
+    }
+
+    @Override
+    public List<Attraction> findByNameSkopje(String name) {
+        return attractionRepository.findByNameSkopje(name);
+    }
+    @Override
+    public List<Attraction> findByNameOhrid(String name) {
+        return attractionRepository.findByNameOhrid(name);
+    }
+
+    @Override
+    public Optional<Attraction> saveSkopje(String lat, String lon, String name, String description,String review,String image) {
+        return attractionRepository.saveSkopje(lat, lon, name, description,review,image);
+    }
+    @Override
+    public Optional<Attraction> saveOhrid(String lat, String lon, String name, String description,String review,String image) {
+        return attractionRepository.saveOhrid(lat, lon, name, description,review,image);
+    }
+
+    @Override
+    public void deleteByIdSkopje(String id) {
+        attractionRepository.deleteByIdSkopje(id);
+    }
+    @Override
+    public void deleteByIdOhrid(String id) {
+        attractionRepository.deleteByIdOhrid(id);
+    }
+}
+
